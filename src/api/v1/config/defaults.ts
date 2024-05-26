e4acf56f7556612160ba828db4aa40284eafc5c1
Brandon Martin <SomeBusinessEmail@protonmail.com>
2024-05-25 22:25:24 -0400

    Added START_TRAINING_MESSAGE to defaults

--- src/api/v1/config/defaults.ts
+++ src/api/v1/config/defaults.ts
@@ -1,3 +1,5 @@
 export const PORT = process.env.PORT || 3000;
 
 export const API_URL = "/api/v1";
+
+export const START_TRAINING_MESSAGE = "DISREGARD THIS MESSAGE: A training is starting! Please make sure to join the training center now! (This is a test of the /training endpoint for my API)";
