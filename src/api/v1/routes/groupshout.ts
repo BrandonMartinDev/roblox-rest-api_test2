187af685611690364afd9d8082b060948e6e420b
Brandon Martin <SomeBusinessEmail@protonmail.com>
2024-05-25 22:26:39 -0400

    Handled invalid groupshout endpoints

--- src/api/v1/routes/groupshout.ts
+++ src/api/v1/routes/groupshout.ts
@@ -4,6 +4,9 @@
 import express from 'express';
 const groupShoutRouter = express.Router();
 
+// Import services methods
+import { RespondWithError } from '@v1services/respondService.js'
+
 // Import controller methods
 import {
     GetGroupShout,
@@ -12,14 +15,22 @@ import {
 
 
 
+
 // -- == [[ ROUTE ENDPOINT METHODS TO CONTROLLERS ]] == -- \\
 
-// /api/v1/groupshout
+// /api/v1/groupshout/
 groupShoutRouter.route('/')
     // GET /api/v1/groupshout
     .get(GetGroupShout)
     // POST /api/v1/groupshout
     .post(ChangeGroupShout);
+
+// /api/v1/groupshout/?
+groupShoutRouter.route('*')
+    // GET /api/v1/groupshout
+    .get((req, res) => {
+        RespondWithError(res, "INVALID_ENDPOINT", 404);
+    })
 
 
 
