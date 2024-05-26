2d81ebaff46c70fcfc986ad29e973a62a051f027
Brandon Martin <SomeBusinessEmail@protonmail.com>
2024-05-25 20:22:20 -0400

    Created /api/v1/ router

---
+++ src/api/v1/routes/index.ts
@@ -0,0 +1,37 @@
+// -- == [[ CONFIG ]] == -- \\
+
+// Import express
+import express from 'express';
+
+// Import Services
+import {
+    RespondWithError,
+    RespondWithSuccess
+} from '@v1services/respondService.js';
+
+// Create v1 Router | ENDPOINT: /api/v1/
+const v1Router = express.Router();
+
+// Import routers
+import groupShoutRouter from '@v1routes/groupshout.js';
+
+
+
+// -- == [[ ROUTE ROUTERS ]] == -- \\
+
+v1Router.use('/groupshout', groupShoutRouter);
+
+
+
+// -- == [[ CONFIG DEFAULT ENDPOINTS ]] == -- \\
+
+// GET /api/v1/
+v1Router.get('*', (req, res) => {
+    RespondWithError(res, 'INVALID_ENDPOINT', 404)
+})
+
+
+
+// -- == [[ EXPORT v1Router | ENDPOINT: /api/v1/ ]] == -- \\
+
+export default v1Router;
