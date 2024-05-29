7373e38183c4cc914a956fd798e765b71722d9d7
Brandon Martin <SomeBusinessEmail@protonmail.com>
2024-05-29 15:54:56 -0400

    Updated defaults to include a timestamp

--- src/api/v1/config/defaults.ts
+++ src/api/v1/config/defaults.ts
@@ -2,4 +2,4 @@ export const PORT = process.env.PORT || 3000;
 
 export const API_URL = "/api/v1";
 
-export const START_TRAINING_MESSAGE = "DISREGARD THIS MESSAGE: A training is starting! Please make sure to join the training center now! (This is a test of the /training endpoint for my API)";
+export const START_TRAINING_MESSAGE = `A training is starting! Please make sure to join the training center now! (This is a test of the /training endpoint for my API): ${new Date().toUTCString()}`;
