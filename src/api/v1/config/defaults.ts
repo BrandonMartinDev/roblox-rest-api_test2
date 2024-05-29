e7c6fa6f177ff9f87c8db09b91bacbbc75be5d6a
Brandon Martin <SomeBusinessEmail@protonmail.com>
2024-05-29 18:18:45 -0400

    Updated default training message

--- src/api/v1/config/defaults.ts
+++ src/api/v1/config/defaults.ts
@@ -2,6 +2,6 @@ export const PORT = process.env.PORT || 3000;
 
 export const API_URL = "/api/v1";
 
-export const START_TRAINING_MESSAGE = `A training is starting! Please make sure to join the training center now! (This is a test of the /training endpoint for my API): ${new Date().toUTCString()}`;
+export const START_TRAINING_MESSAGE = 'A training is starting! Please make sure to join the training center now! (This is a test of the /training endpoint for my API)';
 
-export const RATE_LIMIT = 5; // Amount of requests a client can send per minute
+export const RATE_LIMIT = 10; // Amount of requests a client can send per minute
